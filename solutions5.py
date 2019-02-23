b1ddae03cc528aeb55a3584d940cade07b26de34
tangdaye <tangdaye1996@hotmail.com>
2019-02-23 13:42:55 +0800

    leetcode 43-50

--- solutions5.py
+++ solutions5.py
@@ -74,6 +74,19 @@ def question42(height):
     return trap(height)
 
 
+"""
+43. 字符串相乘
+给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
+示例 1:
+输入: num1 = "2", num2 = "3"
+输出: "6"
+
+示例 2:
+输入: num1 = "123", num2 = "456"
+输出: "56088"
+"""
+
+
 def question43(num1, num2):
     if len(num1) > len(num2):
         num2 = "0" * (len(num1) - len(num2)) + num2
@@ -109,5 +122,188 @@ def question43(num1, num2):
     return "0"
 
 
+"""
+44. 匹配字符串
+给定一个字符串 (s) 和一个字符模式 (p) ，实现一个支持 '?' 和 '*' 的通配符匹配。
+
+'?' 可以匹配任何单个字符。
+'*' 可以匹配任意字符串（包括空字符串）。
+两个字符串完全匹配才算匹配成功。
+
+说明:
+s 可能为空，且只包含从 a-z 的小写字母。
+p 可能为空，且只包含从 a-z 的小写字母，以及字符 ? 和 *。
+
+示例:
+输入:
+s = "adceb"
+p = "*a*b"
+输出: true
+解释: 第一个 '*' 可以匹配空字符串, 第二个 '*' 可以匹配字符串 "dce".
+"""
+
+
+def question44(s, p):
+    n = len(s)
+    m = len(p)
+    last = [False] * (n + 1)
+    last[0] = True
+    for i in range(m):
+        if p[i] == '*':
+            for j in range(n):
+                last[j + 1] = last[j + 1] or last[j]
+        elif p[i] == '?':
+            del last[-1]
+            last.insert(0, False)
+        else:
+            cur = [False]
+            for j in range(n):
+                cur.append(last[j] and p[i] == s[j])
+            last = cur
+    return last[-1]
+
+
+"""
+45. 跳跃游戏II
+给定一个非负整数数组，你最初位于数组的第一个位置。
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+
+示例:
+输入: [2,3,1,1,4]
+输出: 2
+解释: 跳到最后一个位置的最小跳跃数是 2。
+     从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+"""
+
+
+def question45(nums):
+    def find_index(nums, end, index_list):
+        if end == 0:
+            return
+        i = 0
+        while i <= end - 1:
+            if i + nums[i] >= end:
+                index_list.append(i)
+                break
+            i += 1
+        find_index(nums, end=i, index_list=index_list)
+        return
+
+    n = len(nums)
+    if n == 1:
+        return 0
+
+    end, index_list = n - 1, []
+    find_index(nums, end, index_list)
+    return len(index_list)
+
+
+"""
+46. 全排列
+给定一个没有重复数字的序列，返回其所有可能的全排列。
+"""
+
+
+def question46(nums):
+    length = len(nums)
+    if length == 0:
+        return []
+    nums.sort()
+    result = [[nums[0]]]
+    for i in range(1, length):
+        for l in result:
+            if len(l) == i:
+                for j in range(i):
+                    temp = l.copy()
+                    temp.insert(j, nums[i])
+                    result.append(temp)
+                l.append(nums[i])
+    return result
+
+
+"""
+47. 全排列II
+给定一个可包含重复数字的序列，返回所有不重复的全排列
+"""
+
+
+def question47(nums):
+    length = len(nums)
+    if length == 0:
+        return []
+    nums.sort()
+    result = [[nums[0]]]
+    for i in range(1, length):
+        for l in result:
+            if len(l) == i:
+                for j in range(i):
+                    temp = l.copy()
+                    temp.insert(j, nums[i])
+                    result.append(temp)
+                l.append(nums[i])
+    t = []
+    for l in result:
+        if l not in t:
+            t.append(l)
+    return t
+
+
+"""
+48. 旋转图像
+给定一个 n × n 的二维矩阵表示一个图像。
+将图像顺时针旋转 90 度。
+
+说明：
+你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+"""
+
+
+def question48(matrix):
+    n = len(matrix)
+    if n == 1:
+        return
+    else:
+        # 第i行放到倒数第i列上
+        matrix[:] = [[matrix[n - i - 1][j] for i in range(n)] for j in range(n)]
+
+
+"""
+49. 字母异位词分组
+给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+示例:
+输入: ["eat", "tea", "tan", "ate", "nat", "bat"],
+输出:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+"""
+
+
+def question49(strs):
+    result_map = {}
+    for word in strs:
+        word_list = list(word)
+        word_list.sort()
+        if str(word_list) not in result_map:
+            result_map[str(word_list)] = [word]
+        else:
+            result_map[str(word_list)].append(word)
+    result = list(result_map.values())
+    return result
+
+
+"""
+50. Pow(x, n)
+实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+"""
+
+
+def question50(x, n):
+    return x ** n
+
+
 if __name__ == '__main__':
-    print(question43("9133", "0"))
+    print(question49(["eat", "tea", "tan", "ate", "nat", "bat"]))
