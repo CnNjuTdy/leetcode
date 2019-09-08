3ee07e43fab7ca927926514abd1e468d9b540741
tangdaye <tangdaye@bytedance.com>
2019-09-08 11:56:22 +0800

    update

--- solutions9.py
+++ solutions9.py
@@ -134,13 +134,29 @@ def question83(head):
     return root.next
 
 
+"""
+85. 最大矩形
+给定一个仅包含 0 和 1 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+
+示例：
+输入:
+[
+  ["1","0","1","0","0"],
+  ["1","0","1","1","1"],
+  ["1","1","1","1","1"],
+  ["1","0","0","1","0"]
+]
+
+输出: 6
+"""
+
+
+def question85(matrix):
+    if len(matrix) == 0 or len(matrix[0]) == 0:
+        return 0
+    
+    pass
+
+
 if __name__ == '__main__':
-    node = ListNode(1)
-    node.next = ListNode(1)
-    node.next.next = ListNode(2)
-    # node.next.next.next = ListNode(3)
-    # node.next.next.next.next = ListNode(4)
-    # node.next.next.next.next.next = ListNode(4)
-    # node.next.next.next.next.next.next = ListNode(5)
-    m = question83(node)
-    print(m)
+    pass
